f4d6b17e84ddd81f16925040feb7241835a4d32e
Aditya Sakare <aditya.sakare16@gmail.com>
2021-03-23 15:28:50 +0530

    Program to print 5 times “Marvellous” on screen.

---
+++ Program1.2 .c
@@ -0,0 +1,32 @@
+// Program to print 5 time "Marvellous" on screen 
+
+////////////////////////////////////////////////////////
+//Funcation: Display
+//Input:     nothing     
+//Output:    nothing
+//Description: It is used to display "Marvellous" on screen
+//Date:       03/03/2021
+//Author:     Aditya Padmakar Sakare
+//
+//
+///////////////////////////////////////////////////////
+#include<stdio.h>
+
+void Display()
+{
+	int i = 0;
+	for(int i = 1; i <= 5; i++)
+	{
+		printf("Marvellous\n");
+	}
+
+}
+
+int main()
+{
+	Display();
+	
+	return 0;
+
+
+}
