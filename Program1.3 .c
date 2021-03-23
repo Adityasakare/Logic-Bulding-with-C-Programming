9c9e12d065b2e26460e64df74602161b357a3855
Aditya Sakare <aditya.sakare16@gmail.com>
2021-03-23 15:29:25 +0530

    print 5 to 1 numbers on screen.

---
+++ Program1.3 .c
@@ -0,0 +1,33 @@
+//Accept one number from user and print that number of * on screen
+
+////////////////////////////////////////////////////////
+//Funcation: Display
+//Input:     nothing     
+//Output:    nothing
+//Description: It is used to display "*" on screen
+//Date:       03/03/2021
+//Author:     Aditya Padmakar Sakare
+//
+//
+///////////////////////////////////////////////////////
+
+
+
+
+#include<stdio.h>
+
+void Display()
+{
+	int i = 0;
+	i = 5;
+	for(int i = 5; i>=1;i--)
+	{
+		printf("%d\n",i);
+	}
+ 
+}
+int main()
+{
+	Display();
+	return 0;
+}
